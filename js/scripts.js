d058de30c64f054a43d342ecb31aa2a556cdf9cb
JcJen62 <jcjensen62@gmail.com>
2022-01-27 22:30:24 -0700

    I should be a web dev

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 function toggleMenu() {
     document.getElementById("hamburgerBtn").classList.toggle('open');
     document.getElementById("primaryNav").classList.toggle("open")
+    if(document.getElementById("hamburgerBtn").classList.contains('open')){
+        document.getElementById('form').style.display='none'
+    } else {
+        document.getElementById('form').style.display='flex'
+    }
+    
 }
 
 var x = document.getElementById('hamburgerBtn');
